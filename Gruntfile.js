f7c6496416d446449777d7206bac0ab3e4ffe92a
Joachim <joachim.prinzbach@gmail.com>
2017-02-23 10:16:01 +0100

    Fix watch path to slides

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,7 +130,7 @@ module.exports = function(grunt) {
 				tasks: 'css-core'
 			},
 			html: {
-				files: root.map(path => path + '/*.html')
+				files: root.map(path => path + '/slides/*.html')
 			},
 			markdown: {
 				files: root.map(path => path + '/*.md')
